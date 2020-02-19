e7cc2e8d73e2a92d642969fa5f522f7b00e3e736
hammadab <38509606+hammadab@users.noreply.github.com>
2020-02-19 23:58:25 +0300

    Update INS_serial.py

--- INS_serial.py
+++ INS_serial.py
@@ -1,14 +1,14 @@
-import numpy as np
 import serial
+import struct
 
 ser = serial.Serial("COM13", 115200, timeout=10)  # baud rate = 115200
 
 # set parameter
-ser.write(bytes("MessageId = 459\r\n", 'utf-8'))  # 459 = Acc, Gyro, GPS, Velocity, Time, Status
+ser.write(bytes("MessageId = 475\r\n", 'utf-8'))  # 459 = Acc, Gyro, GPS, euler angles, Time, Status, Velocity
 print(ser.readline())
 
 ser.write(b'State = 1\r\n')
-print(ser.readline())
+print(ser.read(3))
 
 while True:
     if ser.read() != b'\x3D':  # wait for Preamble 1 byte
@@ -22,48 +22,27 @@ while True:
         print("byte zero not received")
         continue
     break
-if MessageId == 459:
-    # Data = "Acc, Gyro, GPS, Velocity, Time, Status"
-    # a = ser.read(12 + 12 + 12 + 12 + 4 + 1)  # Data 53 bytes
-    # Acc_x = ser.read(4)
-    # Acc_y = ser.read(4)
-    # Acc_z = ser.read(4)
-    # Gyro_x = ser.read(4)
-    # Gyro_y = ser.read(4)
-    # Gyro_z = ser.read(4)
-    # GPS_x = ser.read(4)
-    # GPS_y = ser.read(4)
-    # GPS_z = ser.read(4)
-    # Velocity_x = ser.read(4)
-    # Velocity_y = ser.read(4)
-    # Velocity_z = ser.read(4)
-    # Time = ser.read(4)
-    # Status = ser.read(1)
-    Acc_x = int.from_bytes(ser.read(4), byteorder='big')
-    Acc_y = int.from_bytes(ser.read(4), byteorder='big')
-    Acc_z = int.from_bytes(ser.read(4), byteorder='big')
-    Gyro_x = int.from_bytes(ser.read(4), byteorder='big')
-    Gyro_y = int.from_bytes(ser.read(4), byteorder='big')
-    Gyro_z = int.from_bytes(ser.read(4), byteorder='big')
-    GPS_x = int.from_bytes(ser.read(4), byteorder='big')
-    GPS_y = int.from_bytes(ser.read(4), byteorder='big')
-    GPS_z = int.from_bytes(ser.read(4), byteorder='big')
-    Velocity_x = int.from_bytes(ser.read(4), byteorder='big')
-    Velocity_y = int.from_bytes(ser.read(4), byteorder='big')
-    Velocity_z = int.from_bytes(ser.read(4), byteorder='big')
-    Time = int.from_bytes(ser.read(4), byteorder='big')
+if MessageId == 475:
+    Acc_x = struct.unpack('f', ser.read(4))
+    Acc_y = struct.unpack('f', ser.read(4))
+    Acc_z = struct.unpack('f', ser.read(4))
+    Gyro_x = struct.unpack('f', ser.read(4))
+    Gyro_y = struct.unpack('f', ser.read(4))
+    Gyro_z = struct.unpack('f', ser.read(4))
+    GPS_x = struct.unpack('f', ser.read(4))
+    GPS_y = struct.unpack('f', ser.read(4))
+    GPS_z = struct.unpack('f', ser.read(4))
+    roll = struct.unpack('f', ser.read(4))
+    pitch = struct.unpack('f', ser.read(4))
+    yaw = struct.unpack('f', ser.read(4))
+    Time = struct.unpack('f', ser.read(4))
     Status = int.from_bytes(ser.read(1), byteorder='big')
+    Velocity_x = struct.unpack('f', ser.read(4))
+    Velocity_y = struct.unpack('f', ser.read(4))
+    Velocity_z = struct.unpack('f', ser.read(4))
 else:
     print("Incorrect MessageId received")
-# elif MessageId == 211:
-#     Data = "Acc, Gyro, Euler, Time, Status"
-#     a = str(ser.read(12 + 12 + 12 + 4 + 1))  # Data 41 bytes
-# elif MessageId == 199:
-#     Data = "Acc, Gyro, Magn, Time, Status"
-#     b = str(ser.read(12 + 12 + 12 + 4 + 1))  # Data 41 bytes
-# elif MessageId == 248:
-#     Data = "Euler, Latitude Longitude Altitude, Pressure, Time, Status"
-#     c = str(ser.read(12 + 12 + 4 + 4 + 1))  # Data 33 bytes
+
 Checksum = ser.read(2)  # Checksum 2 bytes
 
 ser.write(b'State = 0\r\n')
